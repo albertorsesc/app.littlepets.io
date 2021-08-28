cf87a3fdf771d63de40b6f777ae426ca42f336a4
Alberto Rosas <alberto.rsesc@protonmail.com>
2021-08-28 10:08:11 -0700

    WIP

--- app/Models/SocialAccount.php
+++ app/Models/SocialAccount.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SocialAccount extends Model
 {
-    use HasFactory;
+    protected $fillable = ['user_id', 'name', 'client_id', 'avatar'];
+
+    /* Relations */
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
